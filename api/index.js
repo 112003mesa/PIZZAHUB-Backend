2aec9f6a6077ca332b8a0d4f4af45086cbc12f2b
Mesa <112003mesa@gmail.com>
2026-02-06 17:37:42 +0200

    Prepare backend for Vercel deployment, remove app.listen, add serverless support

--- api/index.js
+++ api/index.js
@@ -35,16 +35,9 @@ app.use("/api/admin", adminRoutes);
 
 /* ================= Database ================= */
 
-connectDB()
+await connectDB()
 
 /* ================= Server ================= */
 
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
 
 export default app;
